58a05467fd4d8c559387b835d6890134269f56b3
Aubrey Portwood <aubreypwd@gmail.com>
2016-05-19 15:27:15 -0700

    Fix tabs
    These stupid tabs have been bugging me in the original fork. So, I fixed
    them and made a few tweaks. The tabs are not all thick and bulky
    anymore, they don't have the dark or white background anymore, and it
    loads faster.

--- theme-extension/devtools.js
+++ theme-extension/devtools.js
@@ -1,10 +1,15 @@
-var xhr = new XMLHttpRequest(),
-	stylesheet = 'stable.css';
+var xhr        = new XMLHttpRequest();
+var stylesheet = 'stable.css';
 
-if (/Chrome\/(\d\d)/.exec(navigator.userAgent)[1] > 46) {
-	stylesheet = 'canary.css';
+// Place super minified of stable.css here when ready to release (gets fastest reload).
+var releaseCSS    = false;
+
+// Load the DEV styles.
+if ( ! releaseCSS ) {
+	xhr.open( "GET", "/" + stylesheet, false );
+	xhr.send();
+
+	chrome.devtools.panels.applyStyleSheet( xhr.responseText );
 }
 
-xhr.open("GET", "/" + stylesheet, false);
-xhr.send();
-chrome.devtools.panels.applyStyleSheet(xhr.responseText);
+chrome.devtools.panels.applyStyleSheet( releaseCSS );
